df0e6c401e42be59ce0dc029ed224fb0fd40a26e
Marco Leonardini <marco_leonardini@hotmail.com>
2020-04-11 17:54:05 -0400

    Added Home Page and initial Structure

--- lib/src/ui/pages/home.page.dart
+++ lib/src/ui/pages/home.page.dart
@@ -7,7 +7,24 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         body: Container(
           constraints: BoxConstraints.expand(),
-          child: null,
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 200.0,
+                color: Colors.blueGrey,
+                child: null,
+              ),
+              Expanded(
+                child: Container(
+                  constraints: BoxConstraints.expand(),
+                  child: Image.network(
+                    'https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=lauren-mancke-aOC7TSLb1o8-unsplash.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
