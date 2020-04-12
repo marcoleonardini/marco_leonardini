6f70a285acdf8575a3cb84663b917d1835c235b2
Marco Leonardini <marco_leonardini@hotmail.com>
2020-04-12 17:04:58 -0400

    Added Drawer on mobile

--- lib/src/ui/pages/home.page.dart
+++ lib/src/ui/pages/home.page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'side_bar.page.dart';
@@ -6,24 +7,40 @@ import 'widgets/body_content.widget.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          constraints: BoxConstraints.expand(),
-          child: Row(
-            children: <Widget>[
-              Container(
-                color: Color(0xff222222),
-                width: 200.0,
-                child: SideBarPage(),
-              ),
-              Expanded(
-                child: BodyContent(),
-              )
-            ],
+    if (kIsWeb)
+      return SafeArea(
+        child: Scaffold(
+          body: Container(
+            constraints: BoxConstraints.expand(),
+            child: Row(
+              children: <Widget>[
+                Container(
+                  color: Color(0xff222222),
+                  width: 200.0,
+                  child: SideBarPage(),
+                ),
+                Expanded(
+                  child: BodyContent(),
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    else {
+      return SafeArea(
+        child: Scaffold(
+          drawer: Container(
+            color: Color(0xff222222),
+            width: 200.0,
+            child: SideBarPage(),
+          ),
+          body: Container(
+            constraints: BoxConstraints.expand(),
+            child: BodyContent(),
+          ),
+        ),
+      );
+    }
   }
 }
