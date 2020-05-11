6538311d19c46d823d63fff16278bdfef7c429ce
Marco Leonardini <marco_leonardini@hotmail.com>
2020-05-11 16:34:45 -0400

    updated About

--- lib/src/constants/string.const.dart
+++ lib/src/constants/string.const.dart
@@ -1,2 +1,9 @@
-String kPersonalDesc =
-    "Marco Leonardini es Ing. de Sistemas con 6 años de experiencia en el área de desarrollo Web y Móvil,  organizador principal de Flutter Devs Bolivia y miembro de GDG La Paz, con estas comunidades promueve y participa en diferentes eventos y activiades, difundiendo nuevas tecnologías y conceptos en el área de desarrollo de software. Es co fundador de AuroraSoft, empresa dedicada a la creación de soluciones inteligentes mediante la aplicación de tecnologías modernas.";
+String kPersonalDesc = ''' App developer.
+Marco Leonardini es Ingeniero de Sistemas con 6 años de experiencia trabajando en el area de desarrollo de aplicacionesd web y móviles.
+
+Durante el ultimo año se volvió un developer entusiasta y fan de Flutter, razón que lo llevo a ser fundador y organizador de la comunidad Flutter Devs Bolivia.
+
+También es miembro de GDG La Paz y otras comunidades, con dichas comunidades promueve y participa en diferentes eventos y actividades, difundiendo nuevas tecnologías y conceptos en el área de desarrollo de software.
+
+Co fundador de AuroraSoft, empresa dedicada a la creación de soluciones inteligentes mediante la aplicación de tecnologías modernas.
+''';
